edda7aeabe2956ea5feb5f8be233933796c6d46e
miliincode <milena0202@icloud.com>
2024-08-18 16:33:50 +0300

    feat: added header-widget and changed folder structure

---
+++ src/widgets/header-widget/index.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+import { Root, Title, Info } from "./styles";
+import pointer from "../../shared/icons/pointer";
+
+export const HeaderWidget: FunctionComponent = () => {
+  return (
+    <Root>
+      <Title>Покемоны API</Title>
+      <Info>
+        <div>{pointer}</div>
+        <p>Нажмите на нужного Покемона</p>
+      </Info>
+    </Root>
+  );
+};
