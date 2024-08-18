8f41ae7d90622f6cc26843b2868a5c1984d8230b
miliincode <milena0202@icloud.com>
2024-08-18 16:04:45 +0300

    feat: prepare app and create api

---
+++ src/app/api/index.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const rootReducer = createSlice({
+  name: "data",
+  initialState: {
+  },
+  reducers: {
+
+  }
+}).reducer
---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <></>
+    </Provider>
+  );
+}
+
+export default App;
